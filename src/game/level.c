676e2a1ace6eeb8c3825a9eee3b45903e127fc66
Marcin Kurczewski <dash@wind.garden>
2024-08-07 11:54:02 +0200

    level: refactor for readability

--- src/game/level.c
+++ src/game/level.c
@@ -8,6 +8,62 @@
 
 #include <assert.h>
 
+static void Level_Read(HANDLE handle, void *target, size_t size);
+static int8_t Level_ReadS8(HANDLE handle);
+static int16_t Level_ReadS16(HANDLE handle);
+static int32_t Level_ReadS32(HANDLE handle);
+static uint8_t Level_ReadU8(HANDLE handle);
+static uint16_t Level_ReadU16(HANDLE handle);
+static uint32_t Level_ReadU32(HANDLE handle);
+
+static void Level_Read(HANDLE handle, void *target, size_t size)
+{
+    DWORD bytes_read;
+    ReadFileSync(handle, target, size, &bytes_read, NULL);
+}
+
+static int8_t Level_ReadS8(HANDLE handle)
+{
+    int8_t result;
+    Level_Read(handle, &result, sizeof(result));
+    return result;
+}
+
+static int16_t Level_ReadS16(HANDLE handle)
+{
+    int16_t result;
+    Level_Read(handle, &result, sizeof(result));
+    return result;
+}
+
+static int32_t Level_ReadS32(HANDLE handle)
+{
+    int32_t result;
+    Level_Read(handle, &result, sizeof(result));
+    return result;
+}
+
+static uint8_t Level_ReadU8(HANDLE handle)
+{
+    uint8_t result;
+    Level_Read(handle, &result, sizeof(result));
+    return result;
+}
+
+static uint16_t Level_ReadU16(HANDLE handle)
+{
+    uint16_t result;
+    Level_Read(handle, &result, sizeof(result));
+    return result;
+}
+
+static uint32_t Level_ReadU32(HANDLE handle)
+{
+    uint32_t result;
+    Level_Read(handle, &result, sizeof(result));
+    return result;
+}
+
 BOOL __cdecl Level_LoadTexturePages(HANDLE handle)
 {
     const bool is_16_bit = g_TextureFormat.bpp >= 16;
@@ -15,9 +71,7 @@ BOOL __cdecl Level_LoadTexturePages(HANDLE handle)
     const int32_t texture_size_8_bit = texture_size * sizeof(uint8_t);
     const int32_t texture_size_16_bit = texture_size * sizeof(uint16_t);
 
-    int32_t num_pages;
-    DWORD bytes_read;
-    ReadFileSync(handle, &num_pages, sizeof(int32_t), &bytes_read, NULL);
+    const int32_t num_pages = Level_ReadS32(handle);
 
     if (g_SavedAppSettings.render_mode == RM_SOFTWARE) {
         for (int32_t i = 0; i < num_pages; i++) {
@@ -25,9 +79,7 @@ BOOL __cdecl Level_LoadTexturePages(HANDLE handle)
                 g_TexturePageBuffer8[i] =
                     game_malloc(texture_size, GBUF_TEXTURE_PAGES);
             }
-            ReadFileSync(
-                handle, g_TexturePageBuffer8[i], texture_size, &bytes_read,
-                NULL);
+            Level_Read(handle, g_TexturePageBuffer8[i], texture_size);
         }
         // skip 16-bit texture pages
         SetFilePointer(
@@ -46,14 +98,14 @@ BOOL __cdecl Level_LoadTexturePages(HANDLE handle)
         SetFilePointer(handle, num_pages * texture_size_8_bit, 0, FILE_CURRENT);
         char *ptr = base;
         for (int32_t i = 0; i < num_pages; i++) {
-            ReadFileSync(handle, ptr, texture_size_16_bit, &bytes_read, NULL);
+            Level_Read(handle, ptr, texture_size_16_bit);
             ptr += texture_size_16_bit;
         }
         HWR_LoadTexturePages(num_pages, base, NULL);
     } else {
         char *ptr = base;
         for (int32_t i = 0; i < num_pages; i++) {
-            ReadFileSync(handle, ptr, texture_size_8_bit, &bytes_read, NULL);
+            Level_Read(handle, ptr, texture_size_8_bit);
             ptr += texture_size_8_bit;
         }
         SetFilePointer(
@@ -68,9 +120,7 @@ BOOL __cdecl Level_LoadTexturePages(HANDLE handle)
 
 BOOL __cdecl Level_LoadRooms(HANDLE handle)
 {
-    DWORD bytes_read;
-
-    ReadFileSync(handle, &g_RoomCount, sizeof(int16_t), &bytes_read, NULL);
+    g_RoomCount = Level_ReadS16(handle);
     if (g_RoomCount > MAX_ROOMS) {
         Shell_ExitSystem("Too many rooms");
         return false;
@@ -82,22 +132,18 @@ BOOL __cdecl Level_LoadRooms(HANDLE handle)
     for (int32_t i = 0; i < g_RoomCount; i++) {
         ROOM_INFO *const r = &g_Rooms[i];
 
-        ReadFileSync(handle, &r->pos.x, sizeof(int32_t), &bytes_read, NULL);
+        r->pos.x = Level_ReadS32(handle);
         r->pos.y = 0;
-        ReadFileSync(handle, &r->pos.z, sizeof(int32_t), &bytes_read, NULL);
+        r->pos.z = Level_ReadS32(handle);
 
-        ReadFileSync(handle, &r->min_floor, sizeof(int32_t), &bytes_read, NULL);
-        ReadFileSync(
-            handle, &r->max_ceiling, sizeof(int32_t), &bytes_read, NULL);
+        r->min_floor = Level_ReadS32(handle);
+        r->max_ceiling = Level_ReadS32(handle);
 
-        int32_t data_size;
-        ReadFileSync(handle, &data_size, sizeof(int32_t), &bytes_read, NULL);
+        const int32_t data_size = Level_ReadS32(handle);
         r->data = game_malloc(sizeof(int16_t) * data_size, GBUF_ROOM_MESH);
-        ReadFileSync(
-            handle, r->data, sizeof(int16_t) * data_size, &bytes_read, NULL);
+        Level_Read(handle, r->data, sizeof(int16_t) * data_size);
 
-        int16_t num_doors;
-        ReadFileSync(handle, &num_doors, sizeof(int16_t), &bytes_read, NULL);
+        const int16_t num_doors = Level_ReadS16(handle);
         if (num_doors <= 0) {
             r->doors = NULL;
         } else {
@@ -105,39 +151,29 @@ BOOL __cdecl Level_LoadRooms(HANDLE handle)
                 sizeof(DOOR_INFO) * num_doors + sizeof(DOOR_INFOS),
                 GBUF_ROOM_DOOR);
             r->doors->count = num_doors;
-            ReadFileSync(
-                handle, r->doors->door, sizeof(DOOR_INFO) * num_doors,
-                &bytes_read, NULL);
+            Level_Read(handle, r->doors->door, sizeof(DOOR_INFO) * num_doors);
         }
 
-        ReadFileSync(handle, &r->z_size, sizeof(int16_t), &bytes_read, NULL);
-        ReadFileSync(handle, &r->x_size, sizeof(int16_t), &bytes_read, NULL);
+        r->z_size = Level_ReadS16(handle);
+        r->x_size = Level_ReadS16(handle);
 
         r->sector = game_malloc(
             sizeof(SECTOR_INFO) * r->z_size * r->x_size, GBUF_ROOM_FLOOR);
         for (int32_t i = 0; i < r->z_size * r->x_size; i++) {
             SECTOR_INFO *const sector = &r->sector[i];
-            ReadFileSync(
-                handle, &sector->idx, sizeof(uint16_t), &bytes_read, NULL);
-            ReadFileSync(
-                handle, &sector->box, sizeof(int16_t), &bytes_read, NULL);
-            ReadFileSync(
-                handle, &sector->pit_room, sizeof(uint8_t), &bytes_read, NULL);
-            ReadFileSync(
-                handle, &sector->floor, sizeof(int8_t), &bytes_read, NULL);
-            ReadFileSync(
-                handle, &sector->sky_room, sizeof(uint8_t), &bytes_read, NULL);
-            ReadFileSync(
-                handle, &sector->ceiling, sizeof(int8_t), &bytes_read, NULL);
+            sector->idx = Level_ReadU16(handle);
+            sector->box = Level_ReadS16(handle);
+            sector->pit_room = Level_ReadU8(handle);
+            sector->floor = Level_ReadS8(handle);
+            sector->sky_room = Level_ReadU8(handle);
+            sector->ceiling = Level_ReadS8(handle);
         }
 
-        ReadFileSync(handle, &r->ambient1, sizeof(int16_t), &bytes_read, NULL);
-        ReadFileSync(handle, &r->ambient2, sizeof(int16_t), &bytes_read, NULL);
-        ReadFileSync(
-            handle, &r->light_mode, sizeof(int16_t), &bytes_read, NULL);
+        r->ambient1 = Level_ReadS16(handle);
+        r->ambient2 = Level_ReadS16(handle);
+        r->light_mode = Level_ReadS16(handle);
 
-        ReadFileSync(
-            handle, &r->num_lights, sizeof(int16_t), &bytes_read, NULL);
+        r->num_lights = Level_ReadS16(handle);
         if (!r->num_lights) {
             r->light = NULL;
         } else {
@@ -145,29 +181,17 @@ BOOL __cdecl Level_LoadRooms(HANDLE handle)
                 sizeof(LIGHT_INFO) * r->num_lights, GBUF_ROOM_LIGHTS);
             for (int32_t i = 0; i < r->num_lights; i++) {
                 LIGHT_INFO *const light = &r->light[i];
-                ReadFileSync(
-                    handle, &light->x, sizeof(int32_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &light->y, sizeof(int32_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &light->z, sizeof(int32_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &light->intensity1, sizeof(int16_t), &bytes_read,
-                    NULL);
-                ReadFileSync(
-                    handle, &light->intensity2, sizeof(int16_t), &bytes_read,
-                    NULL);
-                ReadFileSync(
-                    handle, &light->falloff1, sizeof(int32_t), &bytes_read,
-                    NULL);
-                ReadFileSync(
-                    handle, &light->falloff2, sizeof(int32_t), &bytes_read,
-                    NULL);
+                light->x = Level_ReadS32(handle);
+                light->y = Level_ReadS32(handle);
+                light->z = Level_ReadS32(handle);
+                light->intensity1 = Level_ReadS16(handle);
+                light->intensity2 = Level_ReadS16(handle);
+                light->falloff1 = Level_ReadS32(handle);
+                light->falloff2 = Level_ReadS32(handle);
             }
         }
 
-        ReadFileSync(
-            handle, &r->num_meshes, sizeof(int16_t), &bytes_read, NULL);
+        r->num_meshes = Level_ReadS16(handle);
         if (!r->num_meshes) {
             r->mesh = NULL;
         } else {
@@ -175,27 +199,18 @@ BOOL __cdecl Level_LoadRooms(HANDLE handle)
                 sizeof(MESH_INFO) * r->num_meshes, GBUF_ROOM_STATIC_MESH_INFOS);
             for (int32_t i = 0; i < r->num_meshes; i++) {
                 MESH_INFO *const mesh = &r->mesh[i];
-                ReadFileSync(
-                    handle, &mesh->x, sizeof(int32_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &mesh->y, sizeof(int32_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &mesh->z, sizeof(int32_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &mesh->y_rot, sizeof(int16_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &mesh->shade1, sizeof(int16_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &mesh->shade2, sizeof(int16_t), &bytes_read, NULL);
-                ReadFileSync(
-                    handle, &mesh->static_num, sizeof(int16_t), &bytes_read,
-                    NULL);
+                mesh->x = Level_ReadS32(handle);
+                mesh->y = Level_ReadS32(handle);
+                mesh->z = Level_ReadS32(handle);
+                mesh->y_rot = Level_ReadS16(handle);
+                mesh->shade1 = Level_ReadS16(handle);
+                mesh->shade2 = Level_ReadS16(handle);
+                mesh->static_num = Level_ReadS16(handle);
             }
         }
 
-        ReadFileSync(
-            handle, &r->flipped_room, sizeof(int16_t), &bytes_read, NULL);
-        ReadFileSync(handle, &r->flags, sizeof(uint16_t), &bytes_read, NULL);
+        r->flipped_room = Level_ReadS16(handle);
+        r->flags = Level_ReadU16(handle);
 
         r->bound_active = 0;
         r->bound_left = g_PhdWinMaxX;
@@ -206,13 +221,10 @@ BOOL __cdecl Level_LoadRooms(HANDLE handle)
         r->fx_num = NO_ITEM;
     }
 
-    int32_t floor_data_size;
-    ReadFileSync(handle, &floor_data_size, sizeof(int32_t), &bytes_read, NULL);
+    const int32_t floor_data_size = Level_ReadS32(handle);
     g_FloorData =
         game_malloc(sizeof(int16_t) * floor_data_size, GBUF_FLOOR_DATA);
-    ReadFileSync(
-        handle, g_FloorData, sizeof(int16_t) * floor_data_size, &bytes_read,
-        NULL);
+    Level_Read(handle, g_FloorData, sizeof(int16_t) * floor_data_size);
 
     return true;
 }

walkawayy/TR2X

commit 77060c784e0a69cfee9e79173908e24881532212

room: fix dimension names

src/decomp/decomp.c
@@ -1247,7 +1247,7 @@ int32_t __cdecl Room_FindByPos(
     for (int32_t i = 0; i < g_RoomCount; i++) {
         const ROOM_INFO *const room = &g_Rooms[i];
         if ((x >= (room->pos.x + WALL_L))
-            && (x < (room->pos.x + (room->y_size - 1) * WALL_L))
+            && (x < (room->pos.x + (room->x_size - 1) * WALL_L))
             && (y >= room->max_ceiling) && (y <= room->min_floor)
             && (z >= (room->pos.z + WALL_L))
             && (z < (room->pos.z + (room->x_size - 1) * WALL_L))) {

src/game/camera.c
@@ -242,7 +242,7 @@ void __cdecl Camera_SmartShift(
     const ROOM_INFO *r = &g_Rooms[g_Camera.target.room_num];
     int32_t x_floor = (g_Camera.target.z - r->pos.z) >> WALL_SHIFT;
     int32_t y_floor = (g_Camera.target.x - r->pos.x) >> WALL_SHIFT;
-    int16_t item_box = r->sector[x_floor + y_floor * r->x_size].box;
+    int16_t item_box = r->sector[x_floor + y_floor * r->z_size].box;
     const BOX_INFO *box = &g_Boxes[item_box];
 
     int32_t left = (int32_t)box->left << WALL_SHIFT;
@@ -253,7 +253,7 @@ void __cdecl Camera_SmartShift(
     r = &g_Rooms[target->room_num];
     x_floor = (target->z - r->pos.z) >> WALL_SHIFT;
     y_floor = (target->x - r->pos.x) >> WALL_SHIFT;
-    int16_t camera_box = r->sector[x_floor + y_floor * r->x_size].box;
+    int16_t camera_box = r->sector[x_floor + y_floor * r->z_size].box;
 
     if (camera_box != NO_BOX
         && (target->z < left || target->z > right || target->x < top

src/game/console_cmd.c
@@ -80,12 +80,12 @@ static COMMAND_RESULT Console_Cmd_Teleport(const char *const args)
 
             const int32_t x1 = room->pos.x + WALL_L;
             const int32_t x2 =
-                (room->y_size << WALL_SHIFT) + room->pos.x - WALL_L;
+                (room->x_size << WALL_SHIFT) + room->pos.x - WALL_L;
             const int32_t y1 = room->min_floor;
             const int32_t y2 = room->max_ceiling;
             const int32_t z1 = room->pos.z + WALL_L;
             const int32_t z2 =
-                (room->x_size << WALL_SHIFT) + room->pos.z - WALL_L;
+                (room->z_size << WALL_SHIFT) + room->pos.z - WALL_L;
 
             for (int i = 0; i < 100; i++) {
                 int32_t x = x1 + Random_GetControl() * (x2 - x1) / 0x7FFF;

src/game/creature.c
@@ -85,7 +85,7 @@ void __cdecl Creature_AIInfo(ITEM_INFO *const item, AI_INFO *const info)
         const ROOM_INFO *const r = &g_Rooms[item->room_num];
         const int32_t z_floor = (item->pos.z - r->pos.z) >> WALL_SHIFT;
         const int32_t x_floor = (item->pos.x - r->pos.x) >> WALL_SHIFT;
-        item->box_num = r->sector[z_floor + r->x_size * x_floor].box;
+        item->box_num = r->sector[z_floor + r->z_size * x_floor].box;
         info->zone_num = zone[item->box_num];
     }
 
@@ -93,7 +93,7 @@ void __cdecl Creature_AIInfo(ITEM_INFO *const item, AI_INFO *const info)
         const ROOM_INFO *const r = &g_Rooms[enemy->room_num];
         const int32_t z_floor = (enemy->pos.z - r->pos.z) >> WALL_SHIFT;
         const int32_t x_floor = (enemy->pos.x - r->pos.x) >> WALL_SHIFT;
-        enemy->box_num = r->sector[z_floor + r->x_size * x_floor].box;
+        enemy->box_num = r->sector[z_floor + r->z_size * x_floor].box;
         info->enemy_zone_num = zone[enemy->box_num];
     }
 

src/game/items.c
@@ -141,7 +141,7 @@ void __cdecl Item_Initialise(const int16_t item_num)
 
     const int32_t dx = (item->pos.x - room->pos.x) >> WALL_SHIFT;
     const int32_t dz = (item->pos.z - room->pos.z) >> WALL_SHIFT;
-    const SECTOR_INFO *const sector = &room->sector[dx * room->x_size + dz];
+    const SECTOR_INFO *const sector = &room->sector[dx * room->z_size + dz];
     item->floor = sector->floor << 8;
 
     if (g_SaveGame.bonus_flag && !g_IsDemoLevelType) {

src/game/lara/lara_misc.c
@@ -1546,23 +1546,23 @@ int32_t __cdecl Lara_GetWaterDepth(
             x_floor = 0;
             if (y_floor < 1) {
                 y_floor = 1;
-            } else if (y_floor > r->y_size - 2) {
-                y_floor = r->y_size - 2;
+            } else if (y_floor > r->x_size - 2) {
+                y_floor = r->x_size - 2;
             }
-        } else if (x_floor >= r->x_size - 1) {
-            x_floor = r->x_size - 1;
+        } else if (x_floor >= r->z_size - 1) {
+            x_floor = r->z_size - 1;
             if (y_floor < 1) {
                 y_floor = 1;
-            } else if (y_floor > r->y_size - 2) {
-                y_floor = r->y_size - 2;
+            } else if (y_floor > r->x_size - 2) {
+                y_floor = r->x_size - 2;
             }
         } else if (y_floor < 0) {
             y_floor = 0;
-        } else if (y_floor >= r->y_size) {
-            y_floor = r->y_size - 1;
+        } else if (y_floor >= r->x_size) {
+            y_floor = r->x_size - 1;
         }
 
-        sector = &r->sector[x_floor + y_floor * r->x_size];
+        sector = &r->sector[x_floor + y_floor * r->z_size];
         const int16_t data = Room_GetDoor(sector);
         if (data == (uint8_t)NO_ROOM) {
             break;
@@ -1581,7 +1581,7 @@ int32_t __cdecl Lara_GetWaterDepth(
             }
             const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
             const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-            sector = &r->sector[x_floor + y_floor * r->x_size];
+            sector = &r->sector[x_floor + y_floor * r->z_size];
         }
         return 0x7FFF;
     }
@@ -1595,7 +1595,7 @@ int32_t __cdecl Lara_GetWaterDepth(
         }
         const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
         const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-        sector = &r->sector[x_floor + y_floor * r->x_size];
+        sector = &r->sector[x_floor + y_floor * r->z_size];
     }
     return NO_HEIGHT;
 }
@@ -1704,7 +1704,7 @@ void __cdecl Lara_WaterCurrent(COLL_INFO *const coll)
     const ROOM_INFO *const r = &g_Rooms[g_LaraItem->room_num];
     const int32_t x_floor = (g_LaraItem->pos.z - r->pos.z) >> WALL_SHIFT;
     const int32_t y_floor = (g_LaraItem->pos.x - r->pos.x) >> WALL_SHIFT;
-    g_LaraItem->box_num = r->sector[x_floor + y_floor * r->x_size].box;
+    g_LaraItem->box_num = r->sector[x_floor + y_floor * r->z_size].box;
 
     if (g_Lara.creature == NULL) {
         g_Lara.current_active = 0;

src/game/lot.c
@@ -189,7 +189,7 @@ void __cdecl LOT_CreateZone(ITEM_INFO *const item)
     const ROOM_INFO *const r = &g_Rooms[item->room_num];
     const int32_t x_floor = (item->pos.z - r->pos.z) >> WALL_SHIFT;
     const int32_t y_floor = (item->pos.x - r->pos.x) >> WALL_SHIFT;
-    item->box_num = r->sector[x_floor + y_floor * r->x_size].box;
+    item->box_num = r->sector[x_floor + y_floor * r->z_size].box;
 
     int16_t zone_num = zone[item->box_num];
     int16_t flip_num = flip[item->box_num];

src/game/room.c
@@ -18,11 +18,11 @@ int16_t Room_GetIndexFromPos(const int32_t x, const int32_t y, const int32_t z)
     for (int32_t i = 0; i < g_RoomCount; i++) {
         const ROOM_INFO *const room = &g_Rooms[i];
         const int32_t x1 = room->pos.x + WALL_L;
-        const int32_t x2 = room->pos.x + (room->y_size << WALL_SHIFT) - WALL_L;
+        const int32_t x2 = room->pos.x + (room->x_size << WALL_SHIFT) - WALL_L;
         const int32_t y1 = room->max_ceiling;
         const int32_t y2 = room->min_floor;
         const int32_t z1 = room->pos.z + WALL_L;
-        const int32_t z2 = room->pos.z + (room->x_size << WALL_SHIFT) - WALL_L;
+        const int32_t z2 = room->pos.z + (room->z_size << WALL_SHIFT) - WALL_L;
         if (x >= x1 && x < x2 && y >= y1 && y <= y2 && z >= z1 && z < z2) {
             return i;
         }
@@ -86,7 +86,7 @@ int16_t __cdecl Room_GetTiltType(
         const ROOM_INFO *const room = &g_Rooms[sector->pit_room];
         const int32_t x_floor = (z - room->pos.z) >> WALL_SHIFT;
         const int32_t y_floor = (x - room->pos.x) >> WALL_SHIFT;
-        sector = &room->sector[x_floor + y_floor * room->x_size];
+        sector = &room->sector[x_floor + y_floor * room->z_size];
     }
 
     if ((y + 512 >= (sector->floor << 8)) && sector->idx != 0) {
@@ -113,23 +113,23 @@ SECTOR_INFO *__cdecl Room_GetSector(
             x_floor = 0;
             if (y_floor < 1) {
                 y_floor = 1;
-            } else if (y_floor > r->y_size - 2) {
-                y_floor = r->y_size - 2;
+            } else if (y_floor > r->x_size - 2) {
+                y_floor = r->x_size - 2;
             }
-        } else if (x_floor >= r->x_size - 1) {
-            x_floor = r->x_size - 1;
+        } else if (x_floor >= r->z_size - 1) {
+            x_floor = r->z_size - 1;
             if (y_floor < 1) {
                 y_floor = 1;
-            } else if (y_floor > r->y_size - 2) {
-                y_floor = r->y_size - 2;
+            } else if (y_floor > r->x_size - 2) {
+                y_floor = r->x_size - 2;
             }
         } else if (y_floor < 0) {
             y_floor = 0;
-        } else if (y_floor >= r->y_size) {
-            y_floor = r->y_size - 1;
+        } else if (y_floor >= r->x_size) {
+            y_floor = r->x_size - 1;
         }
 
-        sector = &r->sector[x_floor + y_floor * r->x_size];
+        sector = &r->sector[x_floor + y_floor * r->z_size];
         const int16_t door = Room_GetDoor(sector);
         if (door == (uint8_t)NO_ROOM) {
             break;
@@ -145,7 +145,7 @@ SECTOR_INFO *__cdecl Room_GetSector(
             const ROOM_INFO *const r = &g_Rooms[*room_num];
             const int32_t x_floor = ((z - r->pos.z) >> WALL_SHIFT);
             const int32_t y_floor = ((x - r->pos.x) >> WALL_SHIFT);
-            sector = &r->sector[x_floor + y_floor * r->x_size];
+            sector = &r->sector[x_floor + y_floor * r->z_size];
             if (y < (sector->floor << 8)) {
                 break;
             }
@@ -156,7 +156,7 @@ SECTOR_INFO *__cdecl Room_GetSector(
             const ROOM_INFO *const r = &g_Rooms[sector->sky_room];
             const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
             const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-            sector = &r->sector[x_floor + y_floor * r->x_size];
+            sector = &r->sector[x_floor + y_floor * r->z_size];
             if (y >= (sector->ceiling << 8)) {
                 break;
             }
@@ -181,23 +181,23 @@ int32_t __cdecl Room_GetWaterHeight(
             x_floor = 0;
             if (y_floor < 1) {
                 y_floor = 1;
-            } else if (y_floor > r->y_size - 2) {
-                y_floor = r->y_size - 2;
+            } else if (y_floor > r->x_size - 2) {
+                y_floor = r->x_size - 2;
             }
-        } else if (x_floor >= r->x_size - 1) {
-            x_floor = r->x_size - 1;
+        } else if (x_floor >= r->z_size - 1) {
+            x_floor = r->z_size - 1;
             if (y_floor < 1) {
                 y_floor = 1;
-            } else if (y_floor > r->y_size - 2) {
-                y_floor = r->y_size - 2;
+            } else if (y_floor > r->x_size - 2) {
+                y_floor = r->x_size - 2;
             }
         } else if (y_floor < 0) {
             y_floor = 0;
-        } else if (y_floor >= r->y_size) {
-            y_floor = r->y_size - 1;
+        } else if (y_floor >= r->x_size) {
+            y_floor = r->x_size - 1;
         }
 
-        sector = &r->sector[x_floor + y_floor * r->x_size];
+        sector = &r->sector[x_floor + y_floor * r->z_size];
         room_num = Room_GetDoor(sector);
     } while (room_num != (uint8_t)NO_ROOM);
 
@@ -209,7 +209,7 @@ int32_t __cdecl Room_GetWaterHeight(
             }
             const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
             const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-            sector = &r->sector[x_floor + y_floor * r->x_size];
+            sector = &r->sector[x_floor + y_floor * r->z_size];
         }
         return sector->ceiling << 8;
     } else {
@@ -220,7 +220,7 @@ int32_t __cdecl Room_GetWaterHeight(
             }
             const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
             const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-            sector = &r->sector[x_floor + y_floor * r->x_size];
+            sector = &r->sector[x_floor + y_floor * r->z_size];
         }
         return NO_HEIGHT;
     }
@@ -237,7 +237,7 @@ int32_t __cdecl Room_GetHeight(
         const ROOM_INFO *const r = &g_Rooms[sector->pit_room];
         const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
         const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-        sector = &r->sector[x_floor + y_floor * r->x_size];
+        sector = &r->sector[x_floor + y_floor * r->z_size];
     }
 
     int32_t height = sector->floor << 8;
@@ -639,7 +639,7 @@ int32_t __cdecl Room_GetCeiling(
         const ROOM_INFO *const r = &g_Rooms[f->sky_room];
         const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
         const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-        f = &r->sector[x_floor + y_floor * r->x_size];
+        f = &r->sector[x_floor + y_floor * r->z_size];
     }
 
     int32_t height = f->ceiling << 8;
@@ -678,7 +678,7 @@ int32_t __cdecl Room_GetCeiling(
         const ROOM_INFO *const r = &g_Rooms[f->pit_room];
         const int32_t x_floor = (z - r->pos.z) >> WALL_SHIFT;
         const int32_t y_floor = (x - r->pos.x) >> WALL_SHIFT;
-        f = &r->sector[x_floor + y_floor * r->x_size];
+        f = &r->sector[x_floor + y_floor * r->z_size];
     }
 
     if (!f->idx) {

commit 351faf2b187e91ca9c45b14df92d9847ded6baac

decomp: fix regression in Room_FindByPos
Resolves #140. Regression since 77060c784e0a69cfee9e79173908e24881532212.

src/decomp/decomp.c
@@ -1250,7 +1250,7 @@ int32_t __cdecl Room_FindByPos(
             && (x < (room->pos.x + (room->x_size - 1) * WALL_L))
             && (y >= room->max_ceiling) && (y <= room->min_floor)
             && (z >= (room->pos.z + WALL_L))
-            && (z < (room->pos.z + (room->x_size - 1) * WALL_L))) {
+            && (z < (room->pos.z + (room->z_size - 1) * WALL_L))) {
             return i;
         }
     }
@@ -1270,7 +1270,7 @@ void __cdecl CutscenePlayer_Control(const int16_t item_num)
     Collide_GetJointAbsPosition(item, &pos, 0);
 
     const int16_t room_num = Room_FindByPos(pos.x, pos.y, pos.z);
-    if (room_num != NO_ROOM && item->room_num != room_num) {
+    if (room_num != NO_ROOM_NEG && item->room_num != room_num) {
         Item_NewRoom(item_num, room_num);
     }
 
@@ -1297,7 +1297,7 @@ void __cdecl Lara_Control_Cutscene(const int16_t item_num)
     Collide_GetJointAbsPosition(item, &pos, 0);
 
     const int16_t room_num = Room_FindByPos(pos.x, pos.y, pos.z);
-    if (room_num != NO_ROOM && item->room_num != room_num) {
+    if (room_num != NO_ROOM_NEG && item->room_num != room_num) {
         Item_NewRoom(item_num, room_num);
     }
 

src/game/camera.c
@@ -879,7 +879,7 @@ void __cdecl Camera_UpdateCutscene(void)
         .z = g_LaraItem->pos.z + ((cz * c - cx * s) >> W2V_SHIFT),
     };
     int16_t room_num = Room_FindByPos(campos.x, campos.y, campos.z);
-    if (room_num >= 0) {
+    if (room_num != NO_ROOM_NEG) {
         g_Camera.pos.room_num = room_num;
     }
     Output_AlterFOV(fov);